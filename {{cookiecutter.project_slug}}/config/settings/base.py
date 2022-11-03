84bed12e2a7b47938fb11947307358a1283b4503
Remy <rivals.remy@gmail.com>
2022-11-03 18:33:44 +0100

    fix: add whitenoise middleware to serve static files in production

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -121,6 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#middleware
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
